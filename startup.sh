8a7ee16eeb92f31ea3eeeb144bcbd08838ba10b9
David <waterfq@gmail.com>
2025-07-19 15:43:34 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd /home/site/wwwroot
 export FLASK_APP=your_flask_app.py
-gunicorn --bind 0.0.0.0:5000 wsgi:app
-python your_flask_app.py --host=0.0.0.0 --port=80。
+
+# 使用 Gunicorn 绑定到 80 端口
+gunicorn --bind 0.0.0.0:80 wsgi:app
+
+# 如果你选择直接使用 Flask 运行，也可以这样做：
+#python your_flask_app.py --host=0.0.0.0 --port=80
