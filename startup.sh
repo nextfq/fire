924a95ae230f100f2fd36facc09a6d9ccf4cf88d
David <waterfq@gmail.com>
2025-08-04 13:29:01 +0800

    Delete startup.sh

--- startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/site/wwwroot
-export FLASK_APP=your_flask_app.py
-
-# 使用 Gunicorn 绑定到 80 端口
-gunicorn --bind 0.0.0.0:80 wsgi:app
-
-# 如果你选择直接使用 Flask 运行，也可以这样做：
-#python your_flask_app.py --host=0.0.0.0 --port=80
