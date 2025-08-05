02742542f3b6b723399e4b55b0f051fc733ad9df
David <waterfq@gmail.com>
2025-08-05 12:10:52 +0800

    Update lemon.py

--- lemon.py
+++ lemon.py
@@ -3,7 +3,7 @@ lemon = Flask(__name__)
 
 @lemon.route('/')
 def home():
-    return "Hello, Fuck Azure!"
+    return "Hello, Fucking Azure!"
 
 if __name__ == '__main__':
     lemon.run(host='0.0.0.0', port=8080)  # 🔥Azure  一定要 8080
